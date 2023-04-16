3bd5923ece23cb574555886195e266f8ec35dfac
Juanjerrah <juan.oliveira@avonale.com.br>
2023-04-15 22:58:42 -0300

    feat: continuacao

---
+++ Services/Authentication.Api/Migrations/20230411214100_removingColumns.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authentication.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class removingColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "TokenCreated",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "TokenExpires",
+                table: "Usuarios");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenCreated",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpires",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Services/Diario.Api/Migrations/20230415203645_InitialDiario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Diario.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDiario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    DataInclusao = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DataAlteracao = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diarios");
+        }
+    }
+}
